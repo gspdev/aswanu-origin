013521be89cc545861b6bd0e6600f575231d56a8
mivtec <imivtec@gmail.com>
2017-08-16 18:51:59 +0800

    add observer by mivec

---
+++ app/code/local/Mivec/Orderhook/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+class Mivec_Orderhook_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
---
+++ app/code/local/Mivec/Orderhook/Model/Observer.php
@@ -0,0 +1,48 @@
+<?php
+class Mivec_Orderhook_Model_Observer
+{
+    public function implementOrderStatus($event)
+    {
+        $order = $event->getOrder();
+
+        //set banktransfer can execute only.
+        if ($this->_getPaymentMethod($order) == 'banktransfer') {
+            if ($order->canInvoice())
+                $this->_processOrderStatus($order);
+            $this->_sendmail($order);
+        }
+        return $this;
+    }
+
+    protected function _sendmail($order)
+    {
+        return $order->sendNewOrderEmail();
+    }
+
+    private function _getPaymentMethod($order)
+    {
+        return $order->getPayment()->getMethodInstance()->getCode();
+    }
+
+    private function _processOrderStatus($order)
+    {
+        $invoice = $order->prepareInvoice();
+
+        $invoice->register();
+        Mage::getModel('core/resource_transaction')
+            ->addObject($invoice)
+            ->addObject($invoice->getOrder())
+            ->save();
+        
+        $invoice->sendEmail(true, '');
+        $this->_changeOrderStatus($order);
+        return true;
+    }
+
+    private function _changeOrderStatus($order)
+    {
+        $statusMessage = '';
+        $order->setState(Mage_Sales_Model_Order::STATE_PENDING_PAYMENT, true);
+        $order->save();
+    }
+}
