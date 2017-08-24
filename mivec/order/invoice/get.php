22bff13012b1febd78ce4e097c5d00fd8b2cfdab
imivte <imivtec@gmail.com>
2017-08-24 14:29:15 +0800

    update invoice template

--- mivec/order/invoice/get.php
+++ mivec/order/invoice/get.php
@@ -55,8 +55,9 @@ function getOrderInvoice(Mage_Sales_Model_Order $order)
     return $_invoiceData;
 }
 ?>
-<div style="background: #f6f6f6;">
-    <table width="700" cellspacing="0" cellpadding="0" border="0" align="center" bgcolor="#ffffff" style="font-family: 'Open Sans', sans-serif">
+<p style="text-align: right;padding-right: 200px;"><button id="renderPdf">DOWNLOAD PDF</button></p>
+<div id="print" style="background: #f6f6f6;width:700px;margin:0 auto;">
+    <table width="700" cellspacing="0" cellpadding="0" border="0" align="center" bgcolor="#ffffff" style="font-family: 'Helvetica Neue', Helvetica-Neue, HelveticaNeue, Helvetica, Arial, sans-serif;font-size: 10px;">
         <tbody>
         <tr>
             <td>
@@ -65,25 +66,25 @@ function getOrderInvoice(Mage_Sales_Model_Order $order)
                     <tr style="padding: 0 1px;height:100px;">
                         <td width="200" style="vertical-align: top;padding-top: 20px;">
                             <a href="<?php echo __WEB_BASE__?>" style="display: block;">
-                                <img src="<?php echo __WEB_SKIN__;?>images/logo.png" alt="GSP"
+                                <img src="<?php echo __WEB_SKIN__;?>images/logo.png" alt="Aswanu"
                                      width="165" height="48">
                             </a>
                             <!--head-left-->
-                            <div style="width:200px;padding:0 10px;font-size:10px;line-height: 14px;">
+                            <div style="width:200px;padding:0 10px;font-size:10px;line-height: 16px;">
                                 <h5 style="text-transform: uppercase;padding: 25px 0 0 0;margin:0">Shipping From</h5>
-                                <p style="margin:20px 0 10px 0;">Call Us:<?php echo $_general["tel"]?></p>
-                                <p style="margin:10px auto;">Business Time : <?php echo $_general["business_time"]?></p>
-                                <p style="margin:10px auto;">Email : <?php echo $_general["email"];?></p>
-                                <p style="margin:10px auto;"><a href="<?php echo __WEB_BASE__?>"><?php echo __WEB_BASE__;?></a></p>
+                                <p style="margin:20px 0 10px 0;">Add : Rm.6409 SEG Plaza Huaqiangbei Rd.Futian Shenzhen P.R.C.</p>
+                                <p style="margin:0 auto;">Email : <?php echo $_general["email"];?></p>
+                                <p style="margin: 0 auto;">Tel : <?php echo $_general["tel"]?></p>
+                                <p style="margin:0 auto;"><a href="<?php echo __WEB_BASE__?>" style="color:#000;"><?php echo __WEB_BASE__;?></a></p>
                             </div>
                         </td>
                         <td width="100">
                         </td>
                         <!--head-right-->
-                        <td width="350" style="padding-top: 20px;">
+                        <td width="350" style="padding-top: 20px;vertical-align: top;">
                             <table>
                                 <tr>
-                                    <td>
+                                    <td style="vertical-align: top;">
                                         <table>
                                             <tr>
                                                 <td width="150" style="vertical-align: top;">
@@ -101,9 +102,9 @@ function getOrderInvoice(Mage_Sales_Model_Order $order)
                                             </tr>
                                             <tr>
                                                 <td colspan="2" style="vertical-align: top;">
-                                                    <h5 style="text-transform: uppercase;padding: 25px 0 0 0;margin:0">Shipping To</h5>
+                                                    <h5 style="text-transform: uppercase;padding: 36px 0 0 0;margin:0;font-size:12px;">Shipping To</h5>
                                                     <div style="font-size: 10px;padding:0 12px 0 0;">
-                                                        <p><?php echo $_orderData["shipping_address"];?></p>
+                                                        <p style="line-height:16px;"><?php echo $_orderData["shipping_address"];?></p>
                                                     </div>
                                                 </td>
                                             </tr>
@@ -122,10 +123,10 @@ function getOrderInvoice(Mage_Sales_Model_Order $order)
                         <td></td>
                         <td width="450">
                             <div style="border: 2px solid #000;float:right;width: 200px;font-size:12px;">
-                                <p style="font-weight: bold;margin: 0;padding: 0 10px;text-transform: uppercase">
+                                <p style="font-weight: bold;margin: 0;padding: 4px 10px 20px 10px;text-transform: uppercase">
                                     Total: <span style="font-weight: normal;text-align: right;float: right;text-transform: none"><?php echo $_orderData["amount"]["grand_total"]?></span>
                                 </p>
-                                <p style="font-weight: bold;margin: 0;padding: 0 10px;text-transform: uppercase">
+                                <p style="font-weight: bold;margin: 0;padding: 0 10px 4px 10px;text-transform: uppercase">
                                     Due Date : <span style="font-weight: normal;text-align: right;float: right;text-transform: none"><?php echo $_orderData["due_date"]?></span>
                                 </p>
                             </div>
@@ -151,11 +152,11 @@ function getOrderInvoice(Mage_Sales_Model_Order $order)
                             ?>
                             <tbody bgcolor="#F6F6F6">
                                 <tr>
-                                    <td height="30" align="left" valign="top" style="padding:3px 9px;border-bottom:1px dotted #cccccc"><strong style="font-size:11px"><?php echo $_item["sku"]?></strong></td>
-                                    <td align="left" valign="top" style="font-size:11px;padding:3px 9px;border-bottom:1px dotted #cccccc"><?php echo $_item["name"]?></td>
-                                    <td align="center" valign="top" style="font-size:11px;padding:3px 9px;border-bottom:1px dotted #cccccc"><?php echo $_item["qty"]?></td>
-                                    <td align="center" valign="top" style="font-size:11px;padding:3px 9px;border-bottom:1px dotted #cccccc"><?php echo $_item["price"]?></td>
-                                    <td align="right" valign="top" style="font-size:11px;padding:3px 9px;border-bottom:1px dotted #cccccc"><span class="m_-9064054173632497493price"><?php echo $_item["subtotal"]?></span></td>
+                                    <td height="30" align="left" valign="center" style="padding:3px 9px;border-bottom:1px dotted #cccccc;"><strong style="font-size:11px;"><?php echo $_item["sku"]?></strong></td>
+                                    <td align="left" valign="center" style="font-size:11px;padding:3px 9px;border-bottom:1px dotted #cccccc"><?php echo $_item["name"]?></td>
+                                    <td align="center" valign="center" style="font-size:11px;padding:3px 9px;border-bottom:1px dotted #cccccc"><?php echo (int)$_item["qty"]?></td>
+                                    <td align="center" valign="center" style="font-size:11px;padding:3px 9px;border-bottom:1px dotted #cccccc"><?php echo $_item["price"]?></td>
+                                    <td align="right" valign="center" style="font-size:11px;padding:3px 9px;border-bottom:1px dotted #cccccc"><span class="m_-9064054173632497493price"><?php echo $_item["subtotal"]?></span></td>
                                 </tr>
                             </tbody>
                             <?php
@@ -163,7 +164,7 @@ function getOrderInvoice(Mage_Sales_Model_Order $order)
                             endif;
                             ?>
                         </table>
-                        <table width="100%" border="0" cellspacing="5" cellpadding="0">
+                        <table width="100%" border="0" cellspacing="5" cellpadding="0" style="border-collapse: collapse;">
                           <tfoot style="padding:20px;">
                             <tr class="m_-9064054173632497493subtotal">
                               <td colspan="3" align="right" style="padding:3px 9px">Subtotal</td>
@@ -173,9 +174,9 @@ function getOrderInvoice(Mage_Sales_Model_Order $order)
                               <td colspan="3" align="right" style="padding:3px 9px">Shipping &amp; Handling</td>
                               <td align="right" style="padding:3px 9px"><?php echo $_orderData["amount"]["shipping"]?></td>
                             </tr>
-                            <tr class="m_-9064054173632497493grand_total">
-                              <td colspan="3" align="right" style="padding:3px 9px"><strong>Grand Total</strong></td>
-                              <td align="right" style="padding:3px 9px"><strong><?php echo $_orderData["amount"]["grand_total"]?></strong></td>
+                            <tr class="m_-9064054173632497493grand_total" style="border-top:2px solid #000;">
+                              <td colspan="3" align="right" style="padding:3px 9px;"><strong>Grand Total</strong></td>
+                              <td align="right" style="padding:3px 9px;"><strong><?php echo $_orderData["amount"]["grand_total"]?></strong></td>
                             </tr>
                           </tfoot>
                         </table>
@@ -190,22 +191,22 @@ function getOrderInvoice(Mage_Sales_Model_Order $order)
                     <!--footer-->
                     <tr>
                         <td width="450" style="vertical-align: top;">
-                            <h5 style="text-transform: uppercase;padding: 25px 0 0 10px;">SHIPPING INFO:</h5>
-                            <div style="font-size: 10px;padding:0 12px;">
-                                <p>Shipping Carrier : <?php echo $_orderData["carrier"]['carrier']?><?php //echo $_orderData["amount"]["grand_total"]?></p>
-                                <p>Shipping Cost    : <?php echo $_orderData["amount"]["shipping"]?></p>
-                                <p>Due Date         : <?php echo $_orderData["due_date"]?></p>
+                            <h5 style="text-transform: uppercase;padding: 25px 0 0 10px;font-size: 12px;">SHIPPING INFO:</h5>
+                            <div style="font-size: 10px;padding:0 12px; line-height: 14px;border-right:1px solid #000;">
+                                <p style="margin:20px 0 0 0;">Shipping Carrier : <?php echo $_orderData["carrier"]['carrier']?><?php //echo $_orderData["amount"]["grand_total"]?></p>
+                                <p style="margin:4px 0;">Shipping Cost    : <?php echo $_orderData["amount"]["shipping"]?></p>
+                                <p style="margin:4px 0;">Due Date         : <?php echo $_orderData["due_date"]?></p>
                             </div>
                         </td>
                         <td width="450" style="vertical-align: top;">
-                            <h5 style="text-transform: uppercase;padding: 25px 0 0 10px;">Payment method:</h5>
-                            <div style="font-size: 10px;padding:0 12px;border-left:1px solid #000;">
+                            <h5 style="text-transform: uppercase;padding: 25px 0 0 10px;font-size: 12px;">Payment method:</h5>
+                            <div style="font-size: 10px;padding:0 12px;">
                                 <?php echo $_orderData["payment_info"];?>
                             </div>
                         </td>
                     </tr>
                     <tr>
-                        <td colspan="2" align="center" valign="middle" style="vertical-align: top;"><br>Thank You! Aswanu.com</td>
+                        <td colspan="2" align="center" valign="middle" style="vertical-align: top;font-size: 12px;"><br>Thank You! Aswanu.com</td>
                     </tr>
                 </table>
             </td>
@@ -213,3 +214,50 @@ function getOrderInvoice(Mage_Sales_Model_Order $order)
         </tbody>
     </table>
 </div>
+<script type="text/javascript" src="<?php echo __WEB_BASE__?>/js/html2canvas/html2canvas.js"></script>
+<script type="text/javascript" src="<?php echo __WEB_BASE__?>/js/html2canvas/jsPdf.debug.js"></script>
+<script type="text/javascript">
+
+    var downPdf = document.getElementById("renderPdf");
+
+    downPdf.onclick = function() {
+        html2canvas(document.getElementById("print"), {
+            onrendered:function(canvas) {
+
+                var contentWidth = canvas.width;
+                var contentHeight = canvas.height;
+
+                //一页pdf显示html页面生成的canvas高度;
+                var pageHeight = contentWidth / 592.28 * 841.89;
+                //未生成pdf的html页面高度
+                var leftHeight = contentHeight;
+                //pdf页面偏移
+                var position = 0;
+                //a4纸的尺寸[595.28,841.89]，html页面生成的canvas在pdf中图片的宽高
+                var imgWidth = 595.28;
+                var imgHeight = 592.28/contentWidth * contentHeight;
+
+                var pageData = canvas.toDataURL('image/jpeg', 1.0);
+
+                var pdf = new jsPDF('', 'pt', 'a4');
+
+                //有两个高度需要区分，一个是html页面的实际高度，和生成pdf的页面高度(841.89)
+                //当内容未超过pdf一页显示的范围，无需分页
+                if (leftHeight < pageHeight) {
+                    pdf.addImage(pageData, 'JPEG', 0, 0, imgWidth, imgHeight );
+                } else {
+                    while(leftHeight > 0) {
+                        pdf.addImage(pageData, 'JPEG', 0, position, imgWidth, imgHeight);
+                        leftHeight -= pageHeight;
+                        position -= 841.89;
+                        //避免添加空白页
+                        if(leftHeight > 0) {
+                            pdf.addPage();
+                        }
+                    }
+                }
+                pdf.save('invoice.pdf');
+            }
+        })
+    }
+</script>
