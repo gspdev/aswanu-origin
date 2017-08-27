add9a938a42c68166c43e853da366f67af99b690
mivtec <imivtec@gmail.com>
2017-08-27 23:33:59 +0800

    update AJAX mivec

--- mivec/checkout/cart/update.php
+++ mivec/checkout/cart/update.php
@@ -1,16 +1,42 @@
 <?php
 require 'config.php';
 Mage::getSingleton('core/session', array('name' => 'frontend'));
-//Mage::getSingleton('checkout/session')->setCartWasUpdated(true);
-
 $quote = Mage::getSingleton('checkout/session')->getQuote();
 
 $_productId = $app->getRequest()->getParam("product_id");
 $_qty = $app->getRequest()->getParam("qty");
+$_method = $app->getRequest()->getParam("method");
 
-if (!empty($_productId)):
-    echo updateCart($_productId , $_qty);
+if (!$_method)
+    die("Access Denied");
+
+if ($_method == "update") :
+    if (!empty($_productId)):
+        echo updateCart($_productId , $_qty);
+    endif;
+else :
+    echo addToCart($_productId , $_qty);
 endif;
+
+
+function addToCart($_productId , $_qty)
+{
+    $_product = Mage::getModel("catalog/product")->load($_productId);
+    $_cart = Mage::getModel("checkout/cart");
+    $_cart->addProduct($_product , array("qty"  => $_qty));
+
+    try {
+        if ($_cart->save()) {
+            Mage::getSingleton("checkout/session")->setCartWasUpdated(true);
+            return json_encode(array(
+                "status"    => true,
+                "msg"       => "success"
+            ));
+        }
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
 
 function updateCart($_productId , $_qty)
 {
