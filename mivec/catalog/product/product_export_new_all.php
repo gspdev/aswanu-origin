84ad1366b6499fd5ab520930c6c6c0b4594177c0
bigdylanyou <yhj12@foxmail.com>
2018-01-16 15:50:42 +0800

    edit export

--- mivec/catalog/product/product_export_new_all.php
+++ mivec/catalog/product/product_export_new_all.php
@@ -1,50 +1,51 @@
 <?php
 require 'config.php';
-header("Content-Type: text/html;charset=utf-8"); 
+//header("Content-Type: text/html;charset=utf-8"); 
 error_reporting(E_ALL | E_STRICT);
 define('MAGENTO_ROOT', getcwd());
 //$mageFilename = MAGENTO_ROOT . '/app/Mage.php';
 //require_once $mageFilename;
 Mage::setIsDeveloperMode(true);
 ini_set('display_errors', 1);
+ini_set('memory_limit','512M');
 //Mage::app();
 // $products = Mage::getModel("catalog/product")->getCollection();
 // $products->addAttributeToSelect('For iPhone');
 // $products->addAttributeToFilter('status', 1);//optional for only enabled products
 // $products->addAttributeToFilter('visibility', 4);//optional for products only visible in catalog and search
-$cat = Mage::getModel('catalog/category')->load(173);
+$cat = Mage::getModel('catalog/category')->load(94);
 	if(!$cat->getChildren()){
 		$subcats = $cat->getEntityId();
 	}else{
 		$subcats = $cat->getChildren().','.$cat->getEntityId();
 	}
 
-	    $categoryid = 94;
-		$category = new Mage_Catalog_Model_Category();
-		$category->load($categoryid);
-		$collection = $category->getProductCollection();
-		$collection->addAttributeToSelect('*');    
+	    // $categoryid = 94;
+		// $category = new Mage_Catalog_Model_Category();
+		// $category->load($categoryid);
+		// $collection = $category->getProductCollection();
+		// $collection->addAttributeToSelect('*');    
 	
 	
 //print_r($subcats);exit;
 
 //$fp = fopen('C:/Users/Administrator/Desktop/'.$cat->getName().date("Y-m-d").'.csv', 'w');
-$fp = fopen($categoryid.'-'.date("Y-m-d").'.csv', 'w');
+$fp = fopen($cat->getEntityId().'-'.date("Y-m-d").'.csv', 'w');
 $csvHeader = array("gGoodsSku", "gGoodsCode","gGcCode","gGoodsUpc","gGoodsName","gGbId","gGoodsImg","gGoodsUrl","gGradeId","gRemarks","gNetWeight","gGrossWeight","gLength","gWidth","gHeight","gGpId","gHasChild","gIsGroup","gGoodsStatus","gKeepTime","gDeclaredValue","gDeclaredWeight","gDecCnName","gDecEnName","gCustomsCode");
 fputcsv( $fp, $csvHeader,",");
-// foreach(explode(',',$subcats) as $subCatid){
+ foreach(explode(',',$subcats) as $subCatid){
 	
-	      // $_category = Mage::getModel('catalog/category')->load($subCatid);
-          // $products = Mage::getModel('catalog/category')->load($_category->getEntityId())
-          // ->getProductCollection()
-		  // ->addAttributeToFilter('status', 1)//optional for only enabled products
-          // ->addAttributeToFilter('visibility', 4)//optional for products only visible in catalog and search
-          // ->addAttributeToSelect('*');
+	      $_category = Mage::getModel('catalog/category')->load($subCatid);
+          $products = Mage::getModel('catalog/category')->load($_category->getEntityId())
+          ->getProductCollection()
+		  ->addAttributeToFilter('status', 1)//optional for only enabled products
+          ->addAttributeToFilter('visibility', 4)//optional for products only visible in catalog and search
+          ->addAttributeToSelect('*');
 		  
 		  
-    foreach($collection as $product)  {
+   // foreach($collection as $product)  {
 		  
-	   // foreach ($products as $product){
+	    foreach ($products as $product){
 				$sku = trim($product->getSku());
 				$name = trim($product->getName());
 				$weight = $product->getWeight();
@@ -62,7 +63,7 @@ fputcsv( $fp, $csvHeader,",");
 					$categoryData = 0;
 				}
 				
-				$gGcCode=$categoryid;
+				$gGcCode=$cat->getEntityId();
 				
 				$gGbId=0;
 				$gGoodsUpc=0;
@@ -80,10 +81,10 @@ fputcsv( $fp, $csvHeader,",");
 				$gShippingFee=0;
 				$gDeclaredValue=0;
 				$gDeclaredWeight = 0;
-				$gDecCnName = $des_en;
+				$gDecCnName = $name;
 				$gDecEnName = "英文报关名";
 				$gCustomsCode= "海关编码";
 				fputcsv($fp, array($sku,$sku,$gGcCode,$gGoodsUpc,$name,$gGbId,$goods_img,$goods_url,$gGradeId,$gRemarks,$weight,$gGrossWeight,$gLength,$gWidth,$gHeight,$gGpId,$gHasChild,$gIsGroup,$gGoodsStatus,$gKeepTime,$gDeclaredValue,$gDeclaredWeight,$gDecCnName,$gDecEnName,$gCustomsCode), ",");
 			}
-//}
+}
 fclose($fp);
