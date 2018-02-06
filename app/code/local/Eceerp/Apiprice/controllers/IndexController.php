f3aa117d52b3a87c29970817a8212cb575025d56
bigdylanyou <yhj12@foxmail.com>
2018-02-06 17:08:39 +0800

    edit price

--- app/code/local/Eceerp/Apiprice/controllers/IndexController.php
+++ app/code/local/Eceerp/Apiprice/controllers/IndexController.php
@@ -13,136 +13,128 @@ class Eceerp_Apiprice_IndexController extends Mage_Core_Controller_Front_Action
 		//print_r($sku);exit;
 		//test
 		//$sku = array('4897058384345','4897058384338','4897058384888');
-		
-		//print_r($sku);exit;
+
 		$pages = $this->getRequest()->getParam('page');
 		$rows = $this->getRequest()->getParam('row');
-		$count = Mage::getModel('catalog/category')
-			->getProductCollection()
-			->addAttributeToSelect('*')
-			//->addAttributeToFilter('status', 1)
-			->addAttributeToFilter('visibility', 4);
-		$count =  count($count);
+		$_token = $this->getRequest()->getParam('token');
+		$_tokenList = 'gvikukNhVfCoJqVZoaFy';
+		
+		if (!$_token || $_tokenList != $_token) {
+			die('Access Denied');
+		} 
+		else {
+			$_SESSION['token'] = $_tokenList;
+			$count = Mage::getModel('catalog/category')
+				->getProductCollection()
+				->addAttributeToSelect('*')
+				//->addAttributeToFilter('status', 1)
+				->addAttributeToFilter('visibility', 4);
+			$count =  count($count);
 
-	   if(intval($pages)>1){
-			$page = $pages;
-	   }else{
-			$page = 1;
-	   }
-		if(intval($rows)>20){
-			$row = $rows;
-		}else{
-			$row = 20;
-		}
+		   if(intval($pages)>1){
+				$page = $pages;
+		   }else{
+				$page = 1;
+		   }
+			if(intval($rows)>20){
+				$row = $rows;
+			}else{
+				$row = 20;
+			}
 
-		$lastpage = ceil($count/$row);
-		if(empty($sku)){
-			if($page <= $lastpage) {
-				$list = Mage::getModel('catalog/category')
-					->getProductCollection()
-					->addAttributeToSelect('*')
-					//->addAttributeToFilter('status', 1)
-					->addAttributeToFilter('visibility', 4)
-					->setPage($page, $row);
-				foreach ($list as $ke => $item) {
+			$lastpage = ceil($count/$row);
+			if(empty($sku)){
+				if($page <= $lastpage) {
+					$list = Mage::getModel('catalog/category')
+						->getProductCollection()
+						->addAttributeToSelect('*')
+						//->addAttributeToFilter('status', 1)
+						->addAttributeToFilter('visibility', 4)
+						->setPage($page, $row);
+					foreach ($list as $ke => $item) {
 
-					$product['id'] = $item->getId();
-					$product['sku'] = $item->getSku();
-					$product['price'] = $item->getPrice();
+						$product['id'] = $item->getId();
+						$product['sku'] = $item->getSku();
+						$product['price'] = $item->getPrice();
 
-					$products[] = $product;
+						$products[] = $product;
 
+					}
+				}else{
+					$products=[
+					];
 				}
 			}else{
-				$products=[
+				if(!empty($sku)){
+					
+					foreach(explode(',',$sku) as $skuItem){
+						//echo $skuItem;
+						$product = Mage::getModel('catalog/product')->loadByAttribute('sku',$skuItem);
+						//echo $product->getName();
+						if($product){
+							$products[]=[
+								'id' => $product->getId(),
+								'sku'=> $product->getSku(),
+								'price' => $product->getPrice()
+							];
+						}else{
+							$products=[];
+						}
+					}
+					
+				//	$product = Mage::getModel('catalog/product')->loadByAttribute('sku',$sku);
+					
+				}
+				else{
+					$products=[];
+				}
+
+			}
+			if($products){
+				$result = [
+					'status'     => 1,
+					'message'    => '获取成功',
+					'data'       => $products,
+				];
+			}else{
+				$result = [
+					'status'     => 0,
+					'message'    => '获取失败',
+					'data'       => $products,
 				];
 			}
-		}else{
-			if(!empty($sku)){
-				
-				foreach(explode(',',$sku) as $skuItem){
-					//echo $skuItem;
-					$product = Mage::getModel('catalog/product')->loadByAttribute('sku',$skuItem);
-					//echo $product->getName();
-					if($product){
-						$products[]=[
-							'id' => $product->getId(),
-							'sku'=> $product->getSku(),
-							'price' => $product->getPrice()
-						];
-					}else{
-						$products=[];
-					}
-		        }
-				
-			//	$product = Mage::getModel('catalog/product')->loadByAttribute('sku',$sku);
-				
-			}
-			else{
-				$products=[];
-			}
-
-		}
-		if($products){
-			$result = [
-				'status'     => 1,
-				'message'    => '获取成功',
-				'data'       => $products,
-			];
-		}else{
-			$result = [
-				'status'     => 0,
-				'message'    => '获取失败',
-				'data'       => $products,
-			];
-		}
-		echo json_encode($result,TRUE);
+			echo json_encode($result,TRUE);
+	  }	
 	}
+	
 
 	public function updatePriceAction(){
 
-		$sku = Mage::app()->getRequest()->getParam('sku');
-		//$email = $this->getRequest()->getParam('email');
-		$sku = json_decode($sku,true);
-		$write = Mage::getSingleton("core/resource")->getConnection('core_write');
-		$table = Mage::getSingleton('core/resource')->getTableName('erp_update_price');
-		$customer_id = time();
-		if($sku){
-			foreach($sku as $item){
-				$products = Mage::getModel('catalog/product')->loadByAttribute('sku',$item['sku']);
+		$sku = $this->getRequest()->getParam('sku');
+		$_token = $this->getRequest()->getParam('token');
+        $price = $this->getRequest()->getParam('price');
+		$product = Mage::getModel('catalog/product')->loadByAttribute('sku',$sku);
 
-				if($products){
-					$product = Mage::getModel('catalog/product')->loadByAttribute('sku',$item['sku'])->setPrice($item['price'])->save();
-					$write->insert($table,array('customer_id'=>$customer_id,'sku'=>$item['sku'],'price'=>$products->getPrice(),'modify_price'=>$item['price'],'create_time'=>time()));
-					$result = [
-						'status'     => 1,
-						'message'    => '修改成功',
-					];
+		$_tokenList = 'gvikukNhVfCoJqVZoaFy';
+		
+		if (!$_token || $_tokenList != $_token) {
+			die('Access Denied');
+		} else {
+			    $_SESSION['token'] = $_tokenList;
+				if($price > 0){
+				        $product->setPrice($price);
+				}else{
+						$product->setPrice($product->getPrice());
 				}
-			}
-			$results = $write->select()->from(array('main_table'=>$table))->where('customer_id='.$customer_id);
-			$list = $write->fetchAll($results);
-			foreach($list as $k => $v){
-				$ass[$k]['sku'] =  $v['sku'];
-				$ass[$k]['price'] =  $v['price'];
-				$ass[$k]['modify_price'] =  $v['modify_price'];
-			}
-			// 头部标题
-			$csv_header = ['sku','改之前价格','改之后价格'];
-			$filename = $customer_id.'.csv';
-			//$fujian = $_SERVER['DOCUMENT_ROOT'].'/email/'.$filename;
-			Mage::helper('sku')->export_csv_2($ass,$csv_header,$filename);
-			//Mage::helper('sku')->sendEmail($email,$fujian);
-
-		}else{
-
-			$result['status'] = 0;
-			$result['message'] = 'sku为空';
+				// $pro_price =round($product->getPrice(), 2);
+				// if($price != $pro_price){
+					
+				if($product->save()){
+						 echo 'Sku: '.$product->getSku().' Update Price '.$product->getPrice().' Success!'.'<br/>';
+				}
 		}
-
-
-		echo json_encode($result,TRUE);
-	}
+	
+	 }
 
    
 }
